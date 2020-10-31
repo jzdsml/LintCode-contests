3cfc662783c1a8a315e9a6450f45c1929a24e33f
jzdsml <49078452+jzdsml@users.noreply.github.com>
2020-10-31 01:21:10 -0400

    Update Contest#030_Problem#0964_Food_Set.py

--- Contest#030_Problem#0964_Food_Set.py
+++ Contest#030_Problem#0964_Food_Set.py
@@ -8,7 +8,8 @@ class Solution:
     def getMinCalories(self, lunch, dinner, T):
         import bisect
         ans = math.inf
-        N1, N2 = len(lunch), len(dinner)
+        N1 = len(lunch)
+        N2 = len(dinner)
         for i in range(N1):
             if lunch[i][1] >= T:
                 ans = min(ans, lunch[i][0])
@@ -18,13 +19,13 @@ class Solution:
                 
         lunch.sort(key=lambda x: x[1])
         min_cal = [math.inf] * N1
-        min_cal[N1-1] = lunch[N1-1][0]
-        for i in range(N1-2, -1, -1):
-            min_cal[i] = min(min_cal[i+1], lunch[i][0])
+        min_cal[N1 - 1] = lunch[N1 - 1][0]
+        for i in range(N1 - 2, -1, -1):
+            min_cal[i] = min(min_cal[i + 1], lunch[i][0])
         deli = [item[1] for item in lunch]
 
         for i in range(N2):
-            idx = bisect.bisect_left(deli, T-dinner[i][1], lo=0, hi=len(deli))
+            idx = bisect.bisect_left(deli, T - dinner[i][1], lo=0, hi=len(deli))
             if 0 <= idx < len(deli):
                 ans = min(ans, dinner[i][0] + min_cal[idx])
         return ans if ans < math.inf else -1
