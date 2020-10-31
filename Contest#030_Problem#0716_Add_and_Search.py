06282f58adf0926d2185f6eee888b3d8592f0bf7
jzdsml <49078452+jzdsml@users.noreply.github.com>
2020-10-31 01:18:20 -0400

    Update Contest#030_Problem#0716_Add_and_Search.py

--- Contest#030_Problem#0716_Add_and_Search.py
+++ Contest#030_Problem#0716_Add_and_Search.py
@@ -7,8 +7,8 @@ class Solution:
     def addAndSearch(self, inputs, tests):
         set_tests = set(tests)
         N = len(inputs)
-        for i in range(N-1):
-            for j in range(i+1, N):
+        for i in range(N - 1):
+            for j in range(i + 1, N):
                 if inputs[i] + inputs[j] in set_tests:
                     return True
         return False
